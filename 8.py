21b94f22ba9d9b0c232d505bd07b38a568b121bd
Teji17 <109511240+Teji17@users.noreply.github.com>
2022-07-20 17:56:05 +0530

    Delete 8.py

--- 8.py
+++
@@ -1,9 +0,0 @@
-a = ['you', 'are', 'beautiful']
-maxx = len(a[0])
-temp = a[0]
-for i in a:
-    if (len(i) > maxx):
-        maxx = len(i)
-        temp = i
-print('the longest word is', temp)
-print('length of word is', maxx)
